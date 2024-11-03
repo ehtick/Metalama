a44045601deb9c210658f81d91d37b17f47588c4
GaelFraiteur <gael@postsharp.net>
2024-11-03 12:22:04 +0100

    Fixed artifact collection.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -26,7 +26,7 @@ object DebugBuild : BuildType({
 
     name = "Build [Debug]"
 
-    artifactRules = "+:artifacts/publish/public/**/*=>artifacts/publish/public\n+:artifacts/publish/private/**/*=>artifacts/publish/private\n+:artifacts/testResults/**/*=>artifacts/testResults\n+:artifacts/logs/**/*=>logs\n+%system.teamcity.build.tempDir%/Metalama/CompileTimeTroubleshooting/**/*=>logs/CompileTimeTroubleshooting"
+    artifactRules = "+:artifacts/publish/public/**/*=>artifacts/publish/public\n+:artifacts/publish/private/**/*=>artifacts/publish/private\n+:artifacts/testResults/**/*=>artifacts/testResults\n+:artifacts/logs/**/*=>logs\n+:%system.teamcity.build.tempDir%/Metalama/CompileTimeTroubleshooting/**/*=>logs/CompileTimeTroubleshooting"
 
     params {
         text("BuildArguments", "", label = "Build Arguments", description = "Arguments to append to the 'Build' build step.", allowEmpty = true)
