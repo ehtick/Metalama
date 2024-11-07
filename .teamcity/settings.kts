1344553a74c8b85cf0a5a178b876200173e57e7a
Antonín Procházka <antonin@postsharp.net>
2024-11-07 16:52:28 +0100

    TC script updated.

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -26,13 +26,7 @@ object DebugBuild : BuildType({
 
     name = "Build [Debug]"
 
-    artifactRules = 
-        """
-            +:artifacts/publish/public/**/*=>artifacts/publish/public
-            +:artifacts/publish/private/**/*=>artifacts/publish/private
-            +:artifacts/testResults/**/*=>artifacts/testResults
-            +:artifacts/logs/**/*=>logs
-        """
+    artifactRules = "+:artifacts/publish/public/**/*=>artifacts/publish/public\n+:artifacts/publish/private/**/*=>artifacts/publish/private\n+:artifacts/testResults/**/*=>artifacts/testResults\n+:artifacts/logs/**/*=>logs\n"
 
     params {
         text("BuildArguments", "", label = "Build Arguments", description = "Arguments to append to the 'Build' build step.", allowEmpty = true)
