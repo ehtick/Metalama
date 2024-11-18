d7ba9f22a3b9e2f3299bb1d99b59cd4ce8c9d246
Petr Onderka <petro@postsharp.net>
2024-11-18 11:34:56 +0100

    #35581 Add test and comment for enum parameter default value

---
+++ tests/Metalama.Framework.Tests.Integration/Tests/Aspects/Bugs/EnumParameterDefault.t.cs
@@ -0,0 +1,7 @@
+[Aspect]
+class Foo
+{
+  private void Template(global::Metalama.Framework.Tests.Integration.Tests.Aspects.Bugs.EnumParameterDefault.E p = global::Metalama.Framework.Tests.Integration.Tests.Aspects.Bugs.EnumParameterDefault.E.A)
+  {
+  }
+}
