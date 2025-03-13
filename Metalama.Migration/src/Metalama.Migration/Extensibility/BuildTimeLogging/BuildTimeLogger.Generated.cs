71dd06389bc7cc7f6e6899c5f4f345930c236386
Daniel Balas <daniel@postsharp.net>
2025-03-13 15:00:03 +0100

    #35784 More fixes.

--- Metalama.Migration/src/Metalama.Migration/Extensibility/BuildTimeLogging/BuildTimeLogger.Generated.cs
+++ Metalama.Migration/src/Metalama.Migration/Extensibility/BuildTimeLogging/BuildTimeLogger.Generated.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) 2020-2025 SharpCrafters s.r.o. and contributors.
 // SharpCrafters s.r.o. licenses this file to you under either the MIT license or a proprietary license, depending on the repository from which it was obtained.
 // Refer to LICENSE.md in the repository root for complete details.
- 
+
 // THIS FILE IS T4-GENERATED.
 // To edit, go to Logger.Generated.tt.
 // To transform, run this: "C:\Program Files (x86)\Common Files\Microsoft Shared\TextTemplating\14.0\TextTransform.exe" BuildTimeLogger.Generated.tt
--- Metalama.Patterns/src/Flashtrace/Messages/FormattedMessageBuilder.Generated.cs
+++ Metalama.Patterns/src/Flashtrace/Messages/FormattedMessageBuilder.Generated.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) 2020-2025 SharpCrafters s.r.o. and contributors.
 // SharpCrafters s.r.o. licenses this file to you under either the MIT license or a proprietary license, depending on the repository from which it was obtained.
 // Refer to LICENSE.md in the repository root for complete details.
- 
+
 // THIS FILE IS T4-GENERATED.
 // To edit, go to FormattedMessageBuilder.Generated.tt.
 // To transform, run RunT4.ps1.
