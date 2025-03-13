64b34857e017d9d90ff9ee722709faaaf18e4320
Daniel Balas <daniel@postsharp.net>
2025-03-13 14:46:31 +0100

    Fix headers.

--- Metalama.Framework/src/tests/Metalama.Framework.Tests.TemplateTests/Tests/Syntax/ObjectInitializer/CompileTimeObjectInitializers.cs
+++ Metalama.Framework/src/tests/Metalama.Framework.Tests.TemplateTests/Tests/Syntax/ObjectInitializer/CompileTimeObjectInitializers.cs
@@ -2,7 +2,7 @@
 // SharpCrafters s.r.o. licenses this file to you under either the MIT license or a proprietary license, depending on the repository from which it was obtained.
 // Refer to LICENSE.md in the repository root for complete details.
 
-ng System.Collections.Generic;
+using System.Collections.Generic;
 using Metalama.Framework.Advising;
 using Metalama.Framework.Aspects;
 using System;
--- Metalama.Framework/src/tests/Standalone/GeneratedCodeAnalyzerTests/AspectInGenerated/Program.cs
+++ Metalama.Framework/src/tests/Standalone/GeneratedCodeAnalyzerTests/AspectInGenerated/Program.cs
@@ -2,7 +2,7 @@
 // SharpCrafters s.r.o. licenses this file to you under either the MIT license or a proprietary license, depending on the repository from which it was obtained.
 // Refer to LICENSE.md in the repository root for complete details.
 
-ng AspectInGenerated.Components;
+using AspectInGenerated.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
